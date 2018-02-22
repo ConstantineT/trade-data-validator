572e66c50cb3502687169a6b0c0681c909091cb3
ConstantineT <kiivnv@gmail.com>
2018-02-22 22:22:17 +0100

    Remove security starter
    Signed-off-by: ConstantineT <kiivnv@gmail.com>

--- shutdown-server.sh
+++ shutdown-server.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
-if [ $# -lt 2 ]
-  then
-    echo "Please provide user as a first argument and password as a second argument"
-    exit 1
-fi
-
-COMMAND="curl -s --user $1:$2 -X POST http://localhost:8080/shutdown"
+COMMAND="curl -s -X POST http://localhost:8080/shutdown"
 
 RESPONSE=`$COMMAND`
 echo -e "Response:\n$RESPONSE"
